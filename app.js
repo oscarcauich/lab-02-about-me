96a45bb75190ca5b505bf43f36bb34a6e4d11b10
Oscar Cauich <ocauich@gravitypayments.com>
2017-04-04 14:53:41 -0700

    added prompts for end user questions

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 'use strict';
+alert('Lets Play a game!');
+
+//ask user questions about me and define answer variables
+var progLang = prompt('Am I familiar with PHP programming Language?');
+var profBackg = prompt('Is my prefessional Background in Computer Networking?');
+var carfan = prompt('Do I like cars?');
+var origCountry = prompt('Am I originally from Mexico?');
+var currCompany = prompt('Is my current employeer Gravity Payments?');
