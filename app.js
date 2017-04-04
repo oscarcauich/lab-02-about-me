d6c0e9eccc53e3e17318ea1fa2271c2fc1e59f93
Oscar Cauich <ocauich@gravitypayments.com>
2017-04-04 15:17:17 -0700

    added first  if/else statement lines to valide users first input

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ var profBackg = prompt('Is my prefessional Background in Computer Networking?');
 var carfan = prompt('Do I like cars?');
 var origCountry = prompt('Am I originally from Mexico?');
 var currCompany = prompt('Is my current employeer Gravity Payments?');
+
+//take users input and validate input and give feedback to users
+if(progLang.trim().toLowerCase() == 'yes' || progLang.trim().toLowerCase() == 'y'){
+  alert("I've been coding in PHP for a few years now");
+} else if (progLang.trim().toLowerCase() == 'no' || progLang.trim().toLowerCase() == 'n') {
+  alert("Looks like you did not know, I'm familiar with PHP");
+}else {
+  alert("OOPs I don't know what you typed");
+}
